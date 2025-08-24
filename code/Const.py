c21a5ab1572dcecd5aa061190ecc6b3d56db6834
fgrespamads@hotmail.com <fgrespamads@hotmail.com>
2025-08-24 12:09:07 -0300

    Creating level: background, sound.

--- code/Const.py
+++ code/Const.py
@@ -3,6 +3,25 @@
 COLOR_RED = (250,0,0)
 COLOR_WHITE = (255,255,255)
 
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 1,
+    'Level1Bg1': 1,
+    'Level1Bg2': 1,
+    'Level1Bg3': 1,
+    'Level1Bg4': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 1,
+    'Enemy2': 1,
+
+}
+
 # M
 MENU_OPTION = ('NEW GAME',
                'SCORE',
--- code/Menu.py
+++ code/Menu.py
@@ -12,18 +12,23 @@ class Menu:
 
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./Asset/MenuBg.jpg')
+        self.surf = pygame.image.load('./Asset/MenuBg.jpg').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
+        menu_option = 0
         pygame.mixer_music.load('./Asset/Menu.mp3')  # carrega a música de fundo
         pygame.mixer_music.play(-1)
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)  #
             self.menu_text(100, "SHARK", COLOR_RED, ((WIN_WIDTH / 2), 130))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(40, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 300 + 50 *i))
+                if i == menu_option:
+                    self.menu_text(40, MENU_OPTION[i], COLOR_RED, ((WIN_WIDTH / 2), 300 + 50 * i))
+                else:
+                    self.menu_text(40, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 300 + 50 *i))
             pygame.display.flip()
 
             # Check for all events
@@ -31,6 +36,23 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Fechar Janela
                     quit()  # fechar pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option  += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option  -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
+
+
+
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.Font('./Asset/Amity Jack.ttf', size=text_size)  # aplicando fonte baixada
